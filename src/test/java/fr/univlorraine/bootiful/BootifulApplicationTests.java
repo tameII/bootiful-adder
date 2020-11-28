0815774d3d2d3f94e504bae8809bf28973daf256
robocopv6 <redsirop@gmail.com>
2020-11-28 03:39:08 +0100

    add webEnv in Boot[...]Test

--- src/test/java/fr/univlorraine/bootiful/BootifulApplicationTests.java
+++ src/test/java/fr/univlorraine/bootiful/BootifulApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class BootifulApplicationTests {
 
     @Test
